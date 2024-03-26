dc391cfe0f3ab20c1007561fe653d97c5f4d0c77
Kalutu <kalutudaniel@gmail.com>
2024-03-26 12:25:37 +0300

    Calculator App

--- Projects/01 - Calculator App/index.js
+++ Projects/01 - Calculator App/index.js
@@ -12,32 +12,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const calculate = () => {
     answer = eval(expression);
-    answerDisplay.innerHTML = answer;
+    answerDisplay.textContent = answer;
+    expression = expression + "=";
+    expressionInput.value = expression;
     expression = "";
+  };
+
+  const allClear = () => {
+    expression = "";
+    answer = 0;
+    expressionInput.value = expression;
+    answerDisplay.textContent = answer;
   };
 
   const clear = () => {
     expression = expression.slice(0, -1);
     expressionInput.value = expression;
-  };
-
-  const clearAll = () => {
-    expression = "";
-    expressionInput.value = expression;
     answer = 0;
-    answerDisplay.innerHTML = answer;
+    answerDisplay.textContent = answer;
   };
 
   const buttons = document.querySelectorAll(".padButton");
+
   buttons.forEach((button) =>
     button.addEventListener("click", function () {
       const symbol = this.textContent;
       if (symbol === "=") {
         calculate();
+      } else if (symbol === "AC") {
+        allClear();
       } else if (symbol === "C") {
         clear();
-      } else if (symbol === "AC") {
-        clearAll();
       } else {
         display(symbol);
       }
